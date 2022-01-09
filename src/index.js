5af358e3a3cdaf0206d2f0ec1f258bf915b79f2a
eu-andreevna <eu.mamarina2014@yandex.ru>
2022-01-10 02:09:08 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
+    let nStr = n.toString();
   
+    let resultStr = '';
+    
+    for (let i = 0; i < nStr.length; i += 1) {
+      if (nStr[i] === '-'){
+        resultStr = `${resultStr}`;
+      }
+      else{
+        resultStr = `${nStr[i]}${resultStr}`;
+      }
+        
+    }
+    return Number(resultStr);
 }
